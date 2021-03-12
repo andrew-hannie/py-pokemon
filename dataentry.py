7227e2b8585c6d69a4662fc83461e0d2de05161e
Andrew <ahhannie56@gmail.com>
2021-03-12 02:34:24 -0600

    Removed stuff that wasn't needed, renamed stuff

--- dataentry.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd 
-dataset = pd.read_csv("pokemon.csv")
-print(dataset)
-actualdata = dataset.iloc[:, 3:7]
-
-print(actualdata)
-csvStr = actualdata.to_csv(index=False)
-testFile = open("pokemontest.csv", "w")
-testFile.write(csvStr)
