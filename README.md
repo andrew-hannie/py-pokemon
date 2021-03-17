6624cbd8af9915d5b046eb2c7e90a6c490103e51
Andrew <ahhannie56@gmail.com>
2021-03-16 23:36:33 -0500

    commented stuff and added a readme

---
+++ README.md
@@ -0,0 +1,18 @@
+# py-pokemon
+
+This is just a cute little thing that I wrote for fun when I was in a [Pokemon Draft League](https://docs.google.com/spreadsheets/d/1tYq9kek4JSruns_zL17b9A7yFYldE5EVp_XQIhZyL60/edit?usp=sharing)
+
+Basically the long and short of it is that you use the `main.py` to submit a formatted csv file with whatever types the Pokemon are, and it will spit out a nicely formatted `.txt` file that you can use when you play whoever in your draft leagues in order to not refer to the dual type chart at all times.
+
+### Installation
+1. Clone the repository
+2. Forward your command line to client to the clone'd repository's directory
+3. Run the command `python -u main.py`
+4. Pass it the location of a correctly formatted csv file [*see the csv files in the repository for examples*]
+5. Run it and enjoy!
+
+### Future Additions
+- [ ] tkinter based read and write.
+- [ ] UI
+- [ ] csv writer in proper formatting
+- [ ] full search feature?
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from pkmn_project import Pokemon
 import os
 
 # Use this file to send inputs to the pokemon project class
+# and print it to a very nice text file or whatever file type you want 
 
 def readFile(fileName):
     newFile = open(fileName, "r")
@@ -10,19 +11,20 @@ def readFile(fileName):
         if i[-1:] == "\n":
             i = i[:-1]
         splitStr = i.split(',')
-        while '' in splitStr:
+        while '' in splitStr: # redundant basically
             splitStr.remove('')
         if len(splitStr) == 4:
-            newMon = Pokemon(splitStr[0], splitStr[1], splitStr[2], bool(splitStr[3]))
+            newMon = Pokemon(splitStr[0], splitStr[1], splitStr[2], splitStr[3]) # 2 types /w ability
             monList.append(newMon)
         elif len(splitStr) == 3:
-            newMon = Pokemon(splitStr[0], splitStr[1], splitStr[2])
+            newMon = Pokemon(splitStr[0], splitStr[1], splitStr[2]) # 2 types with no ability 
             monList.append(newMon)
         else:
-            newMon = Pokemon(splitStr[0], splitStr[1])
+            newMon = Pokemon(splitStr[0], splitStr[1]) # monotype mons
             monList.append(newMon)
     newFile.close()
     return monList
+# Simple method for writing to files
 def main():
     print("Syntax for Files:")
     print("Pokemon's Name,Type1,Type2 (if needed)")
